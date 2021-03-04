1d2722f440e4d573777b6510693635f2bb85d5c2
조건희 <rjsgml1677@naver.com>
2021-03-05 00:06:35 +0900

    게시판 Controller, Service, dao등 구현 (~209) 2021-03-05

--- CodeStudySpring/src/main/java/org/zerock/controller/HomeController.java
+++ CodeStudySpring/src/main/java/org/zerock/controller/HomeController.java
@@ -11,21 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -35,5 +28,6 @@ public class HomeController {
 		
 		return "home";
 	}
+	
 	
 }
